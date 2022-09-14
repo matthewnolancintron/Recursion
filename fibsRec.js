6c3c2338f331558c51255626764358e653597384
Matthew Shane Nolan-Cintron <matthewnolancintron@gmai.com>
2022-09-14 13:15:50 -0400

    starting warmup:Fibonacci

---
+++ fibsRec.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * same as fibs but solved recursively.
+ * This can be done in just a couple of lines
+ * (or 1 if you’re crazy,
+ * but don’t consider either of these lengths a requirement…
+ * just get it done).
+ */
+function fibsRec(){
+    //todo:
+}
