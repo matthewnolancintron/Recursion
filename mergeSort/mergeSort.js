256c713bd11d5e2789a38ba128ceaf459685d13f
Matthew Shane Nolan-Cintron <matthewnolancintron@gmai.com>
2022-09-21 14:34:34 -0400

    getting close to a solution for recursive mergesort algorithm issuse seems to be with the final merge of the left and right side need to change how i'm merging code at around line 53

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -20,41 +20,161 @@
 //what's the recursive step?
 // what can be delegated to mergeSort ?
 function mergeSort(arr){
-    /*
-      sort the left half of the array (n>1)
-      sort the right half of the array (n>1)
-      merge halves together
-    */
+  console.log(arr, 'starting array');
+    if(arr.length < 2){
+      return  arr;
+    } else {
+      // split array as evenly as possible
+      let middleIndexOfArray = Math.ceil(arr.length/2);
+      let leftSide = arr.slice(0,middleIndexOfArray);
+      let rightSide = arr.slice(middleIndexOfArray,arr.length);
 
+      console.log(leftSide,'l')
+      console.log(rightSide,'r')
+
+      // sort left by using merge sort
+      let sortedLeftSide = mergeSort(leftSide);
+
+      // sort right using merge sort
+      let sortedRightSide = mergeSort(rightSide);
+
+      // merge
+      let mergedArray = [];
+      
+      // 
+      console.log('starting to merge sortedLeft and sortedRight');
+      
+      console.log('arrays before merging');
+      console.log(`${sortedRightSide}sr`,`${sortedLeftSide}sl`);
+
+      //loop through leftSide
+      //doesn't work with larger array or towards
+      //the last merge
+      for(let i,n = 0; (sortedLeftSide.length < 0 || sortedRightSide.length < 0); i++){
+        if(sortedLeftSide[i] < sortedRightSide[n]){
+          mergedArray.push(sortedLeftSide.shift());
+        } else {
+          mergedArray.push(sortedRightSide.shift());
+          n++;
+        }
+      }
+
+
+      console.log('arrays after merging');
+      console.log(`${sortedRightSide}sr`,`${sortedLeftSide}sl`);
+      console.log(mergedArray,'mergedArray')
+
+
+
+      //sortedRightSide still has elements left over
+      if(sortedRightSide.length > 0){
+        //
+        console.log('elements left over in rightSide')
+        console.log('sortedRightSide',sortedRightSide);
+        console.log(sortedLeftSide,'sortedLeftSide');
+
+
+        //if the last element of the merged array
+        //is less than the first element of the 
+        //right sorted array 
+        if(mergedArray[mergedArray.length-1] < sortedRightSide[0]){
+          //then tack the rightSide array to the end of
+          //the merged array
+          mergedArray.push(...sortedRightSide);
+
+        } else {
+          // put sortedRightSide to the start of the mergedArray
+          console.log(mergedArray,'ma');
+          console.log(sortedRightSide,'sr');
+          mergedArray.unshift(...sortedRightSide);
+          console.log(mergedArray,'ma');
+        }
+      }
+
+      if(sortedLeftSide.length > 0){
+        //
+        console.log('elements left over in leftSide')
+        console.log(sortedLeftSide,'sortedLeftSide');
+        console.log('sortedRightSide',sortedRightSide);
+
+        if(mergedArray[mergedArray.length-1] < sortedLeftSide[0]){
+        //then tack the leftSide array to the end of
+        //the merged array
+          mergedArray.push(...sortedLeftSide);
+
+        } else if (mergedArray[0] > sortedLeftSide[sortedLeftSide.length-1] ) {
+          // put sortedLeftSide to the start of the mergedArray
+          console.log(mergedArray,'ma');
+          console.log(sortedRightSide,'sr');
+          mergedArray.unshift(...sortedLeftSide);
+          console.log(mergedArray,'ma');
+        }
+      }
+
+      console.log(sortedLeftSide,'sl');
+      console.log(sortedRightSide,'sr');
+      console.log(mergedArray,'mergedArray')
+
+      return mergedArray;
+    }
     /**
-     *  sorting the left side:
-     *  break array down in to
-     *  split array in half again
-     *  until each half is a
-     *  single element array or 2 element arrays 
+     * psuedo code from cs50
      * 
-     *  sort 2 element arrays by comparaing values
-     *  when sorted merge with any single element arrays
-     *  compare single element with first and last value
-     *  of 2 element array to see which element/array 
-     *  becomes the first element of the merged array
-     *  do until left side is sorted
-     *  do it recursivlly
+     * on input of n elements
+     * can create multiple arrays along 
+     * the process or bounce values 
+     * between only two arrays
+     * 
+     * if n<2 (base case)
+     *  return
+     * else (recursive step)
+     *  sort the left half of elements
+     *  sort right half of elements  
+     *  merge sorted havles 
      */
 
     /**
-     * sorting the right side:
-     * same as what was done on the left
+     * start with whole array:
+     * split the selected array evenly as possible
+     * select the left sub array
+     * 
+     * do:
+     * if size of array greater than 1
+     * split selected array as evenly as possbile
+     * and select left side
+     * else selected array is of size 1
+     * it's ready for merge
+     * while:
+     * repeat until reaching a selected array
+     * of size one
+     * 
+     * (won't use do while will use recursion)
+     * 
+     * merging:
+     * compare left sub array of size 1
+     * against right sub array of size 1
+     * select the minimum of the two values
+     * add selected to a sorted array
+     * when a list becomes empty
+     * copy all values from remaining array 
+     * into sorted array
+     * 
+     * 
+     * 
+     * 
+     * 
+     * split array into halves
+     * 
      */
-
-    /**
-     * merge left and right side of the sorted arrays
-     * by comparing values from start of left side array
-     * lowest value against the next value in the leftside 
-     * array and value from the right side array
-     * plaing them into a new array lowest value
-     * elements are placed first
-     */
-
-
 }
+
+// basic tests
+// console.log(mergeSort([1]));
+// console.log(mergeSort([2,1]));
+// console.log(mergeSort([3,2,1]));
+// mergeSort([3,2,1]);
+mergeSort([6,5,3,4,2,1]);
+
+// more tests
+// console.log(mergeSort([6,4,2,7,3]));
+// console.log(mergeSort([0,3,1,6,4,5,2]));
